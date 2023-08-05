e059501ccfd5e7dd424df9907ed5b50ecd793620
BaptisteGrimaldi <grimaldi.baptiste@gmail.com>
2023-08-05 20:14:10 +0200

    defilement done

--- src/index.ts
+++ src/index.ts
@@ -26,21 +26,8 @@ async function run() {
     throw new Error("L'élément avec l'identifiant #didomi-notice-agree-button n'a pas été trouvé.");
   }
 
-  //Ya un bug sur la lettre X (sa renvoie vers Y) mais url bonne
+  //Y'a un bug sur la lettre X (sa renvoie vers Y) mais url bonne
   const tabLettre: string[] = ['1', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'Z'];
-
-  // try{
-  //   const pageSuivante = await page.waitForSelector(`a[href="/actions/new-york/2?letter=A"]`,{ timeout: 2000 });
-  //   if(pageSuivante){
-  //     pageSuivante.click();
-  //   }
-
-  // }catch{
-  //   console.log("existe pas")
-  // }
-  // <a href="/actions/new-york/2?letter=A" class="data--set">2</a>
-
-  //Enclenchement bon mais suite à faire.
 
   for (const lettre of tabLettre) {
     const entrepriseLettre = await page.waitForSelector(`a[href="?letter=${lettre}"]`);
@@ -50,20 +37,30 @@ async function run() {
   
       await page.waitForTimeout(2000);
   
-      // Boucle pour parcourir les pages à l'intérieur de chaque lettre
       let numPage = 2;
       while (true) {
         try {
           console.log(numPage,lettre);
-          const pageSuivante = await page.waitForSelector(`a[href="/actions/new-york/${numPage}?letter=${lettre}"]`,{ timeout: 2000 });
-          if (pageSuivante) {
-            pageSuivante.click();
+
+          const pageSuivanteInit = await page.waitForSelector(`a[href="/actions/new-york/${numPage}?letter=${lettre}"]`,{ timeout: 500 });
+          if (pageSuivanteInit) {
+            pageSuivanteInit.click();
             numPage++;
             await page.waitForTimeout(2000);
           }
+
         } catch {
-          console.log("break")
-          break;
+          try{
+            const pageSuivante = await page.waitForSelector(`a[href="/actions/new-york/${numPage}"]`,{ timeout: 500 })
+            if(pageSuivante){
+              pageSuivante.click();
+              numPage++;
+              await page.waitForTimeout(2000);
+            }
+          }
+          catch{
+            break;
+          }
         }
       }
     }
